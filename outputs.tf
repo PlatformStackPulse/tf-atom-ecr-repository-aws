60565f8f0921de774c40fc3fc0c426f256e0a126
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:41:27 +0100

    feat: implement tf-atom-ecr-repository-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "arn" {
+  description = "ARN of the ECR repository"
+  value       = try(aws_ecr_repository.this[0].arn, null)
+}
+
+output "repository_url" {
+  description = "URL of the ECR repository"
+  value       = try(aws_ecr_repository.this[0].repository_url, null)
+}
+
+output "registry_id" {
+  description = "Registry ID of the ECR repository"
+  value       = try(aws_ecr_repository.this[0].registry_id, null)
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,33 @@
-# -----------------------------------------------------------------------------
-# Module-Specific Variables
-#
-# Note: Standard labeling variables (enabled, namespace, tenant, environment,
-# stage, name, delimiter, attributes, tags, label_order, etc.) are provided
-# by context.tf via the tf-label module.
-# -----------------------------------------------------------------------------
+variable "image_tag_mutability" {
+  description = "Tag mutability setting (MUTABLE or IMMUTABLE)"
+  type        = string
+  default     = "IMMUTABLE"
+  validation {
+    condition     = contains(["MUTABLE", "IMMUTABLE"], var.image_tag_mutability)
+    error_message = "image_tag_mutability must be MUTABLE or IMMUTABLE."
+  }
+}
+
+variable "scan_on_push" {
+  description = "Whether to scan images on push"
+  type        = bool
+  default     = true
+}
+
+variable "force_delete" {
+  description = "Delete repository even if it contains images"
+  type        = bool
+  default     = false
+}
+
+variable "encryption_type" {
+  description = "Encryption type (AES256 or KMS)"
+  type        = string
+  default     = null
+}
+
+variable "kms_key_arn" {
+  description = "ARN of KMS key for encryption"
+  type        = string
+  default     = null
+}
